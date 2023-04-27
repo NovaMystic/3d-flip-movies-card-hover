911c5ba344c011ed282f8c0c46467a953bcf9d6c
Mystic <mystic.js899@gmail.com>
2023-04-27 05:11:06 +0300

    first commit

---
+++ src/Cards.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+export default function Cards() {
+  return (
+    <>
+      <div className="container">
+        <div className="flip-card">
+          <div className="flip-card-front">
+            <div className="inner">
+              <img src="https://i.imgur.com/C9DWUDi.png" className="icon" />
+              <h3>avengers</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+                corrupti itaque dolore!
+              </p>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <div className="inner">
+              <img src="https://i.imgur.com/aJauAJV.png" className="icon" />
+              <h3>iron man</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                voluptatum eius quam debitis, sit amet sunt neque ipsum?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card">
+          <div className="flip-card-front">
+            <div className="inner">
+              <img src="https://i.imgur.com/C9DWUDi.png" className="icon" />
+              <h3>avengers</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+                corrupti itaque dolore!
+              </p>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <div className="inner">
+              <img src="https://i.imgur.com/ep0HjjD.png" className="icon" />
+              <h3>thor</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                voluptatum eius quam debitis, sit amet sunt neque ipsum?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card">
+          <div className="flip-card-front">
+            <div className="inner">
+              <img src="https://i.imgur.com/C9DWUDi.png" className="icon" />
+              <h3>avengers</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+                corrupti itaque dolore!
+              </p>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <div className="inner">
+              <img src=" https://i.imgur.com/wROMxVv.png" className="icon" />
+              <h3>spider-man</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                voluptatum eius quam debitis, sit amet sunt neque ipsum?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
